1492841900bba2b3b109a549e0edb6a557570875
BaptisteSadoughi <115137893+BaptisteSadoughi@users.noreply.github.com>
2025-06-02 16:17:02 +0200

    Update TissueMarkers_fecal.R
    Replaced the two functions intersecting bsseq with source("name_of_the_script_to_the_functions").

--- TissueMarkers_fecal.R
+++ TissueMarkers_fecal.R
@@ -130,38 +130,7 @@ data_ttms_fecal_sapajus = data_ttms_fecal[,sampleNames(data_ttms_fecal) %in%  me
 data_ttms_fecal_sapajus_cov5p75 <- filterCpGs(data_ttms_fecal_sapajus, cov = 5, perSample = 0.75,
                                               verbose = FALSE, save = FALSE, file = NULL)
 
-# FUNCTION to convert bsseq list to GRangesList
-bsseq_to_gr <- function(bsseq) {
-  chrom <- seqnames(bsseq)
-  start <- start(bsseq)
-  end <- end(bsseq)
-  gr <- GRanges(seqnames = chrom, ranges = IRanges(start = start, end = end))
-  return(gr)
-}
-
-
-# FUNCTION to intersect and filter common sites among three bsseq objects:
-extract_common_cpgs <- function(bsseq_1, bsseq_2,
-                                     bsseq_common_1_name = "bsseq_1_common", 
-                                     bsseq_common_2_name = "bsseq_2_common") {
-  
-  # Create GRanges objects from bsseq_1, bsseq_2, and bsseq_3
-  bsseq_gr_1 <- bsseq_to_gr(bsseq_1)
-  bsseq_gr_2 <- bsseq_to_gr(bsseq_2)
-  
-  # Find common CpG sites among bsseq_gr_1, bsseq_gr_2, and bsseq_gr_3
-  common_sites <- GenomicRanges::intersect(bsseq_gr_1, bsseq_gr_2)
-  
-  # Extract bsseq objects for the common sites
-  bsseq_common_1 <- subsetByOverlaps(bsseq_1, common_sites)
-  bsseq_common_2 <- subsetByOverlaps(bsseq_2, common_sites)
-  
-  # Return the three resulting bsseq objects as a named list
-  result <- list(bsseq_common_1 = bsseq_common_1, bsseq_common_2 = bsseq_common_2)
-  names(result) <- c(bsseq_common_1_name, bsseq_common_2_name)
-  
-  return(result)
-}
+source("Functions_intersect_bsseq.R")
 
 # Apply function to the bsseq objects
 result_intersect_fecal_species <- extract_common_cpgs(
